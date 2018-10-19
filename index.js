c961914945bd182599e877e5443e6d895f64e397
Melissa Nicolich <melissa.nicolich@gmail.com>
2018-10-19 00:30:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Write your code in this file!
-function scuberGreetingForFeet(numberOfFeet){
+function scuberGreetingForFeet(numberOfFeet) {
   // it's free
   if (numberOfFeet <= 400) {
   return 'this one is on me!'
