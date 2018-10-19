c3f038685dfe4f207c434dcd89c6f1ff339b603a
Melissa Nicolich <melissa.nicolich@gmail.com>
2018-10-19 00:34:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ function scuberGreetingForFeet(numberOfFeet) {
   } else if (numberOfFeet > 2000 && numberOfFeet < 2500) {
     return 'I will gladly take your thirty bucks.'
   } else if (numberOfFeet > 2500) {
-    return 'no can do'
+    return 'No can do'
   }
 }function ternaryCheckCity() {
   
